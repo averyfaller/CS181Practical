bcef638503c7e6498d071afd7662327c8fe9a80d
Avery Faller <avyk37@gmail.com>
2016-04-21 15:00:15 -0400

    More bugs fixes and improvements

--- 4/practical4-code/stub.py
+++ 4/practical4-code/stub.py
@@ -20,19 +20,19 @@ class Learner(object):
         
         # This is the learning rate applied to determine to what extent
         # the new information will override the previous information
-        self.learning_rate = .9
+        self.learning_rate = .95
         
         # This is the discount factor which we will use to determine the importance of 
         # future rewards
         self.discount_factor = .5
 
         # The matrix where we will store Q-scores for (state, action) tuples
-        #self.Q = {}
-        
+        self.epoch_X = []
         self.X = []
         self.y = []
-        self.rf = RandomForestRegressor(n_estimators=30)
+        self.rf = RandomForestRegressor(n_estimators=5)
         self.fitted = False
+        self.epoch_gravity = 1
 
     def reset(self):
         self.last_state  = None
@@ -40,9 +40,20 @@ class Learner(object):
         self.last_action = None
         self.last_reward = 0
         self.train = False
+
+        #self.epoch_X[:,-2] = self.epoch_gravity
+        
+        self.X.extend(self.epoch_X)
+        print self.epoch_gravity
+        print '-------------------'
+        print self.epoch_X
+        self.epoch_X = []
+        
+        self.epoch_gravity = 1
         
         # Refit at the start of each epoch
         if len(self.X) > 0:
+            #print self.X[-1], self.y[-1]
             self.rf.fit(self.X, self.y)
             score = self.rf.score(self.X, self.y)
             self.fitted = True
@@ -60,20 +71,29 @@ class Learner(object):
         # The default score to return
 #        return 0
 
-    def state_action_to_array(self, state, action, prev_state):
+    def state_action_to_array(self, state, action, prev_state, set_epoch_graivty=False):
         if state is not None and prev_state is not None:
             #print state['tree']['top'] - state['tree']['bot']
             #arr = [state['tree']['bot'], state['tree']['top'], state['tree']['dist'], 
             #   state['monkey']['bot'], state['monkey']['top'], state['monkey']['vel'],
             #  action]
             #print state['monkey']['top'] - state['monkey']['bot']
-
-            arr = [(state['monkey']['top'] - 28), 
-                   (state['tree']['top'] - 100) - (state['monkey']['top'] - 28), 
-                   (state['tree']['top'] - 100) - (state['monkey']['top'] - 28)^2,
-                   state['tree']['dist'], state['monkey']['vel'],
-                   prev_state['monkey']['vel'] - state['monkey']['vel'], action * 1]
+            #state['tree']['top'] - (state['monkey']['top'], 
+            # ((state['tree']['top'] - 100) - (state['monkey']['top'] - 56))^2,
+            #print state['monkey']['top'] - state['monkey']['bot']
+            # prev_state['monkey']['vel'] - state['monkey']['vel'], 
             
+            if action == 0:
+                self.epoch_gravity = prev_state['monkey']['vel'] - state['monkey']['vel']
+            
+            arr = [state['monkey']['top'], 
+                   state['tree']['top'], 
+                   ((state['tree']['top'] - 100) - (state['monkey']['top'] - 28))**2,
+                   state['tree']['dist'], 
+                   state['monkey']['vel'],
+                   self.epoch_gravity,
+                   action]
+            #print arr
             return arr
         return None
         
@@ -84,39 +104,20 @@ class Learner(object):
         
         return self.rf.predict(np.array(self.state_action_to_array(state, action, prev_state)).reshape(1, -1))
         
-    
-    def Q_to_string(self, state, action):
-        bin_size = 2
-        if state == None:
-            return None
-        
-        q_string = str(state['tree']['bot']//bin_size) + '_' + \
-            str(state['tree']['top']//bin_size) + '_' + \
-            str(state['tree']['dist']//bin_size) + '_' + \
-            str(state['monkey']['bot']//bin_size) + '_' + \
-            str(state['monkey']['top']//bin_size) + '_' + \
-            str(state['monkey']['vel']) + '_' + \
-            str(action)
-        
-        #print q_string
-        return q_string
+   
     
     def set_Q_score(self, state, action, q, prev_state):
         #self.Q[self.Q_to_string(state, action)] = q
-        arr = self.state_action_to_array(state, action, prev_state)
+        arr = self.state_action_to_array(state, action, prev_state, set_epoch_graivty=True)
         if arr is not None:
-            self.X.append(arr)
+            self.epoch_X.append(arr)
             self.y.append(q)
-            
-
-            
 
     def action_callback(self, state):
         '''
         Implement this function to learn things and take actions.
         Return 0 if you don't want to jump and 1 if you do.
         '''
-
         # You might do some learning here based on the current state and the last state.
 
         # You'll need to select and action and return it.
@@ -126,6 +127,8 @@ class Learner(object):
         swing_Q = prev_Q + self.learning_rate * (self.last_reward + self.discount_factor * self.get_Q_score(state, 0, self.last_state) - prev_Q)
         
         jump_Q = prev_Q + self.learning_rate * (self.last_reward + self.discount_factor * self.get_Q_score(state, 1, self.last_state) - prev_Q)
+
+        print "SWING %f, JUMP %f" % (swing_Q, jump_Q)
         
         # Pick the better Q score from the possible actions at s_{t+1}
         action_Qs = [swing_Q, jump_Q]
@@ -133,30 +136,27 @@ class Learner(object):
         if jump_Q > swing_Q:
             best_action = 1
             
-        if self.train:
-            new_action = npr.rand() < 0.1
+        if self.train or jump_Q == swing_Q:
+            new_action = (npr.rand() < 0.1) * 1
         else:
             new_action = best_action
         
-        #print swing_Q
-        #print jump_Q
         best_Q = action_Qs[new_action]
         
         if isinstance(best_Q, list):
             best_Q = best_Q[0]
+        print best_Q
+        
+        print new_action
         
         # Update the Q score for the last state and action
-        self.set_Q_score(self.last_state, new_action, best_Q, self.penultimate_state)
-        
-        new_action = new_action
-        new_state  = state
+        self.set_Q_score(self.last_state, self.last_action, best_Q, self.penultimate_state)
 
-        self.last_action = new_action
-        
         self.penultimate_state = self.last_state
-        self.last_state  = new_state
+        self.last_state  = state
+        self.last_action = new_action
 
-        return self.last_action
+        return new_action
 
     def reward_callback(self, reward):
         '''This gets called so you can see what reward you get.'''
@@ -203,7 +203,7 @@ if __name__ == '__main__':
     hist = [0]
 
     # Run games. 
-    run_games(agent, hist, 300, 5, 10)
+    run_games(agent, hist, 300, 10, 5)
 
     #print "Num states: %d" % len(agent.Q)
     
