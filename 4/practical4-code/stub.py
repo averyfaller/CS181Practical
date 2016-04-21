85ac6f71fe3124942565d280ad41b8325bfce7b6
Avery Faller <avyk37@gmail.com>
2016-04-21 02:02:59 -0400

    Started work on Practical 4

--- 4/practical4-code/stub.py
+++ 4/practical4-code/stub.py
@@ -1,6 +1,8 @@
 # Imports.
 import numpy as np
 import numpy.random as npr
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 
 from SwingyMonkey import SwingyMonkey
 
@@ -12,13 +14,102 @@ class Learner(object):
 
     def __init__(self):
         self.last_state  = None
+        self.penultimate_state = None
         self.last_action = None
-        self.last_reward = None
+        self.last_reward = 0
+        
+        # This is the learning rate applied to determine to what extent
+        # the new information will override the previous information
+        self.learning_rate = .9
+        
+        # This is the discount factor which we will use to determine the importance of 
+        # future rewards
+        self.discount_factor = .5
+
+        # The matrix where we will store Q-scores for (state, action) tuples
+        #self.Q = {}
+        
+        self.X = []
+        self.y = []
+        self.rf = RandomForestRegressor(n_estimators=30)
+        self.fitted = False
 
     def reset(self):
         self.last_state  = None
+        self.penultimate_state = None
         self.last_action = None
-        self.last_reward = None
+        self.last_reward = 0
+        self.train = False
+        
+        # Refit at the start of each epoch
+        if len(self.X) > 0:
+            self.rf.fit(self.X, self.y)
+            score = self.rf.score(self.X, self.y)
+            self.fitted = True
+            print "Refitting for new epoch, score: %f" % score
+        
+#    def get_Q_score(self, state, action):
+#        if state == None or action == None:
+#            return 0
+        
+#        if str((state, action)) in self.Q:
+#            a_q = self.Q[self.Q_to_string(state, action)]
+#            if a_q is not None:
+#                return a_q
+
+        # The default score to return
+#        return 0
+
+    def state_action_to_array(self, state, action, prev_state):
+        if state is not None and prev_state is not None:
+            #print state['tree']['top'] - state['tree']['bot']
+            #arr = [state['tree']['bot'], state['tree']['top'], state['tree']['dist'], 
+            #   state['monkey']['bot'], state['monkey']['top'], state['monkey']['vel'],
+            #  action]
+            #print state['monkey']['top'] - state['monkey']['bot']
+
+            arr = [(state['monkey']['top'] - 28), 
+                   (state['tree']['top'] - 100) - (state['monkey']['top'] - 28), 
+                   (state['tree']['top'] - 100) - (state['monkey']['top'] - 28)^2,
+                   state['tree']['dist'], state['monkey']['vel'],
+                   prev_state['monkey']['vel'] - state['monkey']['vel'], action * 1]
+            
+            return arr
+        return None
+        
+
+    def get_Q_score(self, state, action, prev_state):
+        if state is None or prev_state is None or action is None or not self.fitted:
+            return 0
+        
+        return self.rf.predict(np.array(self.state_action_to_array(state, action, prev_state)).reshape(1, -1))
+        
+    
+    def Q_to_string(self, state, action):
+        bin_size = 2
+        if state == None:
+            return None
+        
+        q_string = str(state['tree']['bot']//bin_size) + '_' + \
+            str(state['tree']['top']//bin_size) + '_' + \
+            str(state['tree']['dist']//bin_size) + '_' + \
+            str(state['monkey']['bot']//bin_size) + '_' + \
+            str(state['monkey']['top']//bin_size) + '_' + \
+            str(state['monkey']['vel']) + '_' + \
+            str(action)
+        
+        #print q_string
+        return q_string
+    
+    def set_Q_score(self, state, action, q, prev_state):
+        #self.Q[self.Q_to_string(state, action)] = q
+        arr = self.state_action_to_array(state, action, prev_state)
+        if arr is not None:
+            self.X.append(arr)
+            self.y.append(q)
+            
+
+            
 
     def action_callback(self, state):
         '''
@@ -30,30 +121,62 @@ class Learner(object):
 
         # You'll need to select and action and return it.
         # Return 0 to swing and 1 to jump.
-
-        new_action = npr.rand() < 0.1
+        prev_Q = self.get_Q_score(self.last_state, self.last_action, self.penultimate_state)
+        
+        swing_Q = prev_Q + self.learning_rate * (self.last_reward + self.discount_factor * self.get_Q_score(state, 0, self.last_state) - prev_Q)
+        
+        jump_Q = prev_Q + self.learning_rate * (self.last_reward + self.discount_factor * self.get_Q_score(state, 1, self.last_state) - prev_Q)
+        
+        # Pick the better Q score from the possible actions at s_{t+1}
+        action_Qs = [swing_Q, jump_Q]
+        best_action = 0
+        if jump_Q > swing_Q:
+            best_action = 1
+            
+        if self.train:
+            new_action = npr.rand() < 0.1
+        else:
+            new_action = best_action
+        
+        #print swing_Q
+        #print jump_Q
+        best_Q = action_Qs[new_action]
+        
+        if isinstance(best_Q, list):
+            best_Q = best_Q[0]
+        
+        # Update the Q score for the last state and action
+        self.set_Q_score(self.last_state, new_action, best_Q, self.penultimate_state)
+        
+        new_action = new_action
         new_state  = state
 
         self.last_action = new_action
+        
+        self.penultimate_state = self.last_state
         self.last_state  = new_state
 
         return self.last_action
 
     def reward_callback(self, reward):
         '''This gets called so you can see what reward you get.'''
-
         self.last_reward = reward
 
 
-def run_games(learner, hist, iters = 100, t_len = 100):
+def run_games(learner, hist, iters = 100, t_len = 100, r_iters = 10):
     '''
     Driver function to simulate learning by having the agent play a sequence of games.
     '''
+    print "Running %d games" % iters
     
     for ii in range(iters):
         # Make a new monkey object.
+        if ii < r_iters:
+            learner.train = True
+            print "Running a training epoch"
+        
         swing = SwingyMonkey(sound=False,                  # Don't play sounds.
-                             text="Epoch %d" % (ii),       # Display the epoch on screen.
+                             text="Epoch %d, Max Score %d" % (ii, max(hist)) ,       # Display the epoch on screen.
                              tick_length = t_len,          # Make game ticks super fast.
                              action_callback=learner.action_callback,
                              reward_callback=learner.reward_callback)
@@ -73,16 +196,19 @@ def run_games(learner, hist, iters = 100, t_len = 100):
 
 if __name__ == '__main__':
 
-	# Select agent.
-	agent = Learner()
+    # Select agent.
+    agent = Learner()
 
-	# Empty list to save history.
-	hist = []
+    # Empty list to save history.
+    hist = [0]
 
-	# Run games. 
-	run_games(agent, hist, 20, 10)
+    # Run games. 
+    run_games(agent, hist, 300, 5, 10)
 
-	# Save history. 
-	np.save('hist',np.array(hist))
-
-
+    #print "Num states: %d" % len(agent.Q)
+    
+    # Save history. 
+    np.save('hist',np.array(hist))
+    
+    # Print max score
+    print "Max score was: %f" % max(hist)
